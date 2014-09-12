f15c7859ad0438a25f4a955fd72c468028d677c6
Somenath Ghosh <somenath.ghosh84@gmail.com>
2014-09-11 21:22:11 -0400

    v0.0.1

--- app.js
+++ app.js
@@ -62,4 +62,10 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  log.info('Express server listening on port ' + server.address().port);
+});
+
+//module.exports = app;
